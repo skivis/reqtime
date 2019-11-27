76c431304b39db64ba8fa965a94fc483ca98e58c
Siim Kivistik <siim.kivistik@panagora.se>
2019-11-27 14:45:15 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reqtime',
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['reqtime'],
     include_package_data=True,
     install_requires=[
