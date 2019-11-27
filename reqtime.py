8263baa9f5e2e1bfd451431bf3d57a4f8b7ac8a9
Siim Kivistik <siim.kivistik@panagora.se>
2019-11-27 14:35:16 +0100

    Refactoring and add support for POST requests aswell, WIP

--- reqtime.py
+++ reqtime.py
@@ -1,25 +1,46 @@
 from statistics import median, mean
-from timeit import default_timer
+from timeit import default_timer as timer
 
 import requests
-from click import command, option, argument, style, echo
+from click import command, option, argument, style, echo, UsageError
 from tabulate import tabulate
 
 
+SUPPORTED_HTTP_METHODS = ['GET', 'POST', 'EOD']
+
+
+def log(status: int, elapsed: float, threshold: int):
+    output = f'{elapsed:9.2f}'
+    if threshold > 0:
+        color = 'bright_green' if int(elapsed) <= threshold else 'bright_red'
+        output = style(output, fg=color)
+    
+    echo(f'({style(str(status), fg="bright_black")}) {output}')
+
 
 @command()
+@argument('args', nargs=-1)
 @option('-c', '--count', default=0, type=int, help='Number of requests to run, defaults to infinite')
 @option('-t', '--threshold', default=0, type=int, help='Threshold in ms for marking a request as slow')
 @option('-p', '--persistent', is_flag=True, help='Use a persistent http connection for all requests')
 @option('-s', '--summary', is_flag=True, help='Output summary when done (or stopped)')
 @option('-v', '--verbose', is_flag=True, help='Turn on DEBUG logging')
-@argument('url')
-def cli(count, threshold, persistent, summary, verbose, url):
+def cli(args, count, threshold, persistent, summary, verbose):
+    if not args or len(args) > 2:
+        raise UsageError('Incorrect argument, either specify <METHOD> <url> or just <url>')
+
+    try:
+        method, url = args
+        if method not in SUPPORTED_HTTP_METHODS:
+            raise UsageError(f'Unsupported HTTP method: {method}')
+    except ValueError:
+        method = 'GET'
+        url = args[0]
+    
     if verbose:
         import logging
         logging.basicConfig(level=logging.DEBUG)
-        for package in ['urllib3', 'requests']:
-            logging.getLogger(package).setLevel(logging.DEBUG)
+        logging.getLogger('urllib3').setLevel(logging.DEBUG)
 
     if persistent:
         http = requests.Session()
@@ -29,21 +50,15 @@ def cli(count, threshold, persistent, summary, verbose, url):
     durations = []
     
     try:
-        start = default_timer()
+        func = getattr(http, method.lower())
+        start = timer()
         index = count
         while True:
-            response = http.get(url)
-            elapsed = response.elapsed.total_seconds() * 1000
-            status = response.status_code
+            r = func(url)
+            elapsed = r.elapsed.total_seconds() * 1000
             durations.append(elapsed)
-            output = f'{elapsed:.2f}'
-            if threshold > 0:
-                color = 'bright_green' if int(elapsed) <= threshold else 'bright_red'
-                output = style(f'{output:^14}', fg=color)
-            else:
-                output = f'{output:^14}'
             
-            echo(f'({style(str(status), fg="bright_black")}) {output}')
+            log(r.status_code, elapsed, threshold)
             
             if count == 0:
                 continue
@@ -53,7 +68,7 @@ def cli(count, threshold, persistent, summary, verbose, url):
     except KeyboardInterrupt:
         pass
     finally:
-        end = default_timer()
+        end = timer()
         if hasattr(http, 'close'):
             http.close()
         
